39118e17c5a3d049eff2e92a1fd7895887367e48
parvin <parvinnoori0@gmail.com>
2024-01-18 13:02:38 +0330

    category loading

--- src/CategoryList/category-list.jsx
+++ src/CategoryList/category-list.jsx
@@ -1,32 +1,43 @@
 import React, { useEffect, useState } from "react";
 import axios from "../axios";
+import Loading from "../Loading/loading";
 
 export default function CategoryList() {
   const [categories, setCategories] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const fetchCategories = async () => {
       const response = await axios.get("/FoodCategory/categories");
       setCategories(response.data);
+      setLoading(false);
     };
-    fetchCategories()
+    fetchCategories();
   }, []);
   return (
     <nav className="container mt-n5">
       <div
         className="d-flex align-items-center bg-white rounded-3 shadow-lg py-4"
         style={{ height: "80px" }}
->
-        <ul className="nav">
-          <li className="nav-item">
-            <a href="" className="nav-link">همه فست فودها</a>
-          </li>
-          {categories.map(category=>(
-            <li className="nav-item" key={category.id}>
-                <a href="" className="nav-link">{category.name}</a>
+      >
+        {loading ? (
+          <Loading theme="primary" />
+        ) : (
+          <ul className="nav">
+            <li className="nav-item">
+              <a href="" className="nav-link">
+                همه فست فودها
+              </a>
             </li>
-          ))}
-        </ul>
+            {categories.map((category) => (
+              <li className="nav-item" key={category.id}>
+                <a href="" className="nav-link">
+                  {category.name}
+                </a>
+              </li>
+            ))}
+          </ul>
+        )}
       </div>
     </nav>
   );
---
+++ src/Loading/loading.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Loading({theme}) {
+  return (
+   <div className="d-flex justify-content-center m-auto">
+    <div className={`loading spinner-border text-${theme || 'success'}`}></div>
+   </div>
+  )
+}
