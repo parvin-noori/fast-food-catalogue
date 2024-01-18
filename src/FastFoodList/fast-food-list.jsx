f7676e822d1607f48f7d8a255c9e83a6fc468f0b
parvin <parvinnoori0@gmail.com>
2024-01-18 13:31:11 +0330

    fast food items

--- src/FastFoodList/fast-food-list.jsx
+++ src/FastFoodList/fast-food-list.jsx
@@ -1,7 +1,16 @@
-import React from 'react'
+import React from "react";
+import FastFoodItem from "../FastFoodItem/fast-food-item";
 
-export default function FastFoodList({fastFoodItems}) {
+export default function FastFoodList({ fastFoodItems }) {
   return (
-    <div>fastFoodList</div>
-  )
+    <div className="container">
+      <div className="row">
+        {fastFoodItems.map((item) => (
+          <div className="col-md-4 col-sm-6 mb-grid-gutter" key={item.id}>
+            <FastFoodItem {...item} />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
